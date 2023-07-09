e7ad234092aa7b5d829c216794e92f6511de696c
Om Mani Tripathi <76935835+OmMani002@users.noreply.github.com>
2023-07-09 22:46:49 +0530

    1

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -34,7 +34,7 @@ const Portfolio = () => {
     <div name="portfolio" className="bg-gradient-to-b from-black via-black to-gray-800 w-full text-white md:h-screen">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                <p className="text-6xl font-bold inline border-b-4 border-gray-500">
                     Projects
                 </p>
                 <p className="py-6">Some of my works</p>
