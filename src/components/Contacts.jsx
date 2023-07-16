a944546b3736264eb79f7b4419ed91815af4cf81
Om Mani Tripathi <76935835+OmMani002@users.noreply.github.com>
2023-07-16 18:55:50 +0530

    changed lets talk color

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -29,7 +29,7 @@ const Contacts = () => {
                     rows="10" className="p-2 bg-transparent 
                     border-2 rounded-md text-white focus:outline-none"></textarea>
 
-                    <button className="text-white bg-gradient-to-b from-blue-200 to-green-400 
+                    <button className="text-white bg-gradient-to-b from-blue-200 to-green-800 
                     px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"> Let's Talk</button>
                 </form>
             </div>
